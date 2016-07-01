1aab02335d61dc74a31b7a26792a16fae94043cf
Bruno Bachmann <bfbachmann@gmail.com>
2016-06-30 21:17:21 -0700

    added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+	has_many :comments
 	validates :title, presence: true, length: {minimum: 2, maximum: 50}
 	validates :text, presence: true, length: {minimum: 140}
 end
